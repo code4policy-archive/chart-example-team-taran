b40c79283cb6a98b57d55c50fa573b7b3aa79545
Taran Raghuram <taran.raghuram@gmail.com>
2022-01-12 08:16:49 -0500

    Parts 3 & 4 complete

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,99 +1,106 @@
-var margin = {top: 20, right: 50, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+function drawLineChart(cssSelector){
 
-var parseDate = d3.time.format("%d-%b-%y").parse,
-    bisectDate = d3.bisector(function(d) { return d.date; }).left,
-    formatValue = d3.format(",.2f"),
-    formatCurrency = function(d) { return "$" + formatValue(d); };
+        var margin = {top: 20, right: 50, bottom: 30, left: 50},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
 
-var x = d3.time.scale()
-    .range([0, width]);
+        var parseDate = d3.time.format("%d-%b-%y").parse,
+            bisectDate = d3.bisector(function(d) { return d.date; }).left,
+            formatValue = d3.format(",.2f"),
+            formatCurrency = function(d) { return "$" + formatValue(d); };
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+        var x = d3.time.scale()
+            .range([0, width]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+        var y = d3.scale.linear()
+            .range([height, 0]);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
 
-var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
 
-var svg = d3.select("#apple-stock-chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .attr("class", "chart")
-    .attr("id", "apple-stock-chart")
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var line = d3.svg.line()
+            .x(function(d) { return x(d.date); })
+            .y(function(d) { return y(d.close); });
 
-d3.tsv("data.tsv", function(error, data) {
-  if (error) throw error;
+        var svg = d3.select(cssSelector).append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .attr("class", "chart")
+            .attr("id", cssSelector)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.close = +d.close;
-  });
+        d3.tsv("data.tsv", function(error, data) {
+          if (error) throw error;
 
-  data.sort(function(a, b) {
-    return a.date - b.date;
-  });
+          data.forEach(function(d) {
+            d.date = parseDate(d.date);
+            d.close = +d.close;
+          });
 
-  x.domain([data[0].date, data[data.length - 1].date]);
-  y.domain(d3.extent(data, function(d) { return d.close; }));
+          data.sort(function(a, b) {
+            return a.date - b.date;
+          });
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+          x.domain([data[0].date, data[data.length - 1].date]);
+          y.domain(d3.extent(data, function(d) { return d.close; }));
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Price ($)");
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
 
-  svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
+          svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Price ($)");
 
-  var focus = svg.append("g")
-      .attr("class", "focus")
-      .style("display", "none");
+          svg.append("path")
+              .datum(data)
+              .attr("class", "line")
+              .attr("d", line);
 
-  focus.append("circle")
-      .attr("r", 4.5);
+          var focus = svg.append("g")
+              .attr("class", "focus")
+              .style("display", "none");
 
-  focus.append("text")
-      .attr("x", 9)
-      .attr("dy", ".35em");
+          focus.append("circle")
+              .attr("r", 4.5);
 
-  svg.append("rect")
-      .attr("class", "overlay")
-      .attr("width", width)
-      .attr("height", height)
-      .on("mouseover", function() { focus.style("display", null); })
-      .on("mouseout", function() { focus.style("display", "none"); })
-      .on("mousemove", mousemove);
+          focus.append("text")
+              .attr("x", 9)
+              .attr("dy", ".35em");
 
-  function mousemove() {
-    var x0 = x.invert(d3.mouse(this)[0]),
-        i = bisectDate(data, x0, 1),
-        d0 = data[i - 1],
-        d1 = data[i],
-        d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-    focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
-    focus.select("text").text(formatCurrency(d.close));
-  }
-});
+          svg.append("rect")
+              .attr("class", "overlay")
+              .attr("width", width)
+              .attr("height", height)
+              .on("mouseover", function() { focus.style("display", null); })
+              .on("mouseout", function() { focus.style("display", "none"); })
+              .on("mousemove", mousemove);
+
+          function mousemove() {
+            var x0 = x.invert(d3.mouse(this)[0]),
+                i = bisectDate(data, x0, 1),
+                d0 = data[i - 1],
+                d1 = data[i],
+                d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+            focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
+            focus.select("text").text(formatCurrency(d.close));
+          }
+        });
+
+}
+
+drawLineChart('#apple-stock-chart')
+drawLineChart('#apple-stock-chart-2')
